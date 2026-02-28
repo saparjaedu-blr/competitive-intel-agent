4f82a138ea135765c9533a679f78a934979c4ae8
saparjaedu-blr <saparja.edu@gmail.com>
2026-02-28 23:57:36 +0530

    Update synthesizer.py

--- agent/nodes/synthesizer.py
+++ agent/nodes/synthesizer.py
@@ -1,5 +1,5 @@
 from langchain_openai import ChatOpenAI
-from langchain.schema import HumanMessage, SystemMessage
+from langchain_core.messages import HumanMessage, SystemMessage
 from agent.state import AgentState, CompetitorSynthesis
 from config.settings import OPENAI_API_KEY, OPENAI_MODEL
 
